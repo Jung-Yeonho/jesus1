e7dc78fbd66e7fce8ebff771e1343a9c8f93f512
Jung-Yeonho <89023169+Jung-Yeonho@users.noreply.github.com>
2021-08-30 23:19:04 +0900

    Add files via upload

--- colors.js
+++ colors.js
@@ -1,19 +1,22 @@
 var Links = {
   setColor: function(color){
-    alist = document.querySelectorAll('a');
-    var i = 0;
-    while(i < alist.length){
-      alist[i].style.color = color;
-        i = i + 1
-        }
+    // alist = document.querySelectorAll('a');
+    // var i = 0;
+    // while(i < alist.length){
+    //   alist[i].style.color = color;
+    //     i = i + 1
+    //     }
+    $('a').css('color',color);
   }
 }
   var Body = {
     setColor:function(color){
-      document.querySelector('body').style.color = color;
+      // document.querySelector('body').style.color = color;
+      $('body').css('color', color);
     },
     setBackgroundColor:function(color){
-       document.querySelector('body').style.backgroundColor = color;
+       // document.querySelector('body').style.backgroundColor = color;
+       $('body').css('backgroundColor', color);
     }
   }
   function nightDayHandler(self){
@@ -22,7 +25,7 @@ var Links = {
     Body.setBackgroundColor('black');
     Body.setColor('white');
     self.value = 'day';
-    Links.setColor('powderblue');
+    Links.setColor('yellow');
 
     }else{
     Body.setBackgroundColor('white');
